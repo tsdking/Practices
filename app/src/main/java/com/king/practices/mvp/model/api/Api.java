8884b2ff2d98270f45e348829e8b892418e7f3d9
tsdking <xqflylg@gmail.com>
2017-09-02 13:26:27 +0800

    Initial commit

---
+++ app/src/main/java/com/king/practices/mvp/model/api/Api.java
@@ -0,0 +1,8 @@
+package com.king.practices.mvp.model.api;
+
+/**
+ * Created by admin on 2017/9/2.
+ */
+
+public interface Api {
+}
---
+++ app/src/main/java/com/king/practices/mvp/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.king.practices.mvp.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.king.practices.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
